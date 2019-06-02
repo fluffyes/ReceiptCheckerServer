32a3c4049798c1f17dedf9c127fd4a7ad138d7d4
Axel <soulchildsince2013@gmail.com>
2019-06-03 01:49:54 +0800

    fixed stuff

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'venice'
 
 get '/' do
   "Please send a 64 base encoded receipt file to POST '/'"  
@@ -21,7 +22,13 @@ post '/' do
   receipt_data = request_payload['receipt']
 
   if receipt = Venice::Receipt.verify(receipt_data)
-    return json({ message: "original_application_version (build number) is #{receipt.original_application_version.to_i}"})
+    original_build_number = receipt.original_application_version.to_i
+
+    if original_build_number <= 42
+      # provide premium feature if build number is smaller than the free with IAP build
+    end
+
+    return json({ message: "original_application_version (build number) is #{original_build_number}"})
   else
     status 400
     return json({ message: 'please supply a valid receipt'})
